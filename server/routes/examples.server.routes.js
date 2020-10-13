ecc89c02a3a10c1fcc69cf186d33bc3b45547672
AllieWu <44093216+AllieWu@users.noreply.github.com>
2020-10-13 16:46:17 -0400

    test

--- server/routes/examples.server.routes.js
+++ server/routes/examples.server.routes.js
@@ -1,8 +1,9 @@
-const examples = require('../controllers/examples.server.controller.js'),
-    express = require('express'), 
-    router = express.Router()
+const examples = require("../controllers/examples.server.controller.js"),
+  express = require("express"),
+  router = express.Router();
 
-router.route('/')
-  .get(examples.hello);
-  
+//router.route('/')
+// .get(examples.hello);
+router.get("/", examples.hello);
+
 module.exports = router;
