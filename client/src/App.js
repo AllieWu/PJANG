7c4b9cfa3b5e5c6d828d34864c0832c0cd1edf3a
AllieWu <wu.allison@ufl.edu>
2020-10-31 19:14:04 -0400

    Finished the tracking shopping cart task

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import Product from "./views/Product/Product"
-import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
+import React, { useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Home from "./views/Home/Home";
+import Product from "./views/Product/Product";
+import NotFound from "./views/NotFound";
+import Header from "./components/Header/Header";
 
 const App = () => {
-
+  const [itemsInCart, setItemsInCart] = useState([]);
   const [page, setPage] = useState([{}]);
 
   return (
@@ -17,13 +17,16 @@ const App = () => {
         <Route exact path="/">
           <Redirect to="/Home" />
         </Route>
-        <Route path="/Product/:pageNumber"
-          render={(props) => { return <Product {...props} page={page} setPage={setPage} />}}
+        <Route
+          path="/Product/:pageNumber"
+          render={(props) => {
+            return <Product {...props} page={page} setPage={setPage} />;
+          }}
         />
-        <Route component={NotFound}/>
+        <Route component={NotFound} />
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
