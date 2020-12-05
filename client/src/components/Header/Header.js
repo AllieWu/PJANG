9af08874c51716219aace9b1ab8abba438b6b706
AllieWu <wu.allison@ufl.edu>
2020-12-05 15:48:12 -0500

    styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,37 +4,61 @@ import "./Header.css";
 
 import ShoppingCartButton from "./../../components/ShoppingCartButton/ShoppingCartButton.js";
 import laundrLogo from "./../../assets/laundrLogo.png";
+import blueLaundrLogo from "./../../assets/blueCombine.png";
 
-const Header = (props) => {
-  return (
-    <div className="topnav">
-      <Link id="logo-link" to="/">
-        <img className="topnav-logo" src={laundrLogo} alt="Laundr logo" />
-      </Link>
-      <div className="topnav-right align">
-        <ShoppingCartButton
-            style={{ top: "50", left: "0" }}
-            itemsInCart={props.itemsInCart}
-            onAddToCartClick={props.handleAddToCartClick}
-            onRemoveFromCartClick={props.handleRemoveFromCartClick}
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  routeChange(path) {
+    window.location.pathname = path;
+  }
+
+  render() {
+    return (
+      <div className="topnav">
+        <Link id="logo-link" to="/">
+          <img
+            className="topnav-logo"
+            src={this.props.page === "home" ? laundrLogo : blueLaundrLogo}
+            alt="Laundr Logo"
           />
-        {props.currentUser ?
-          (
-              <span>
-                  <Link className ="topnav-link" to='/History'>History</Link>
-                  <Link className ="topnav-link" to='/Logout'>Log Out</Link>
-              </span>
-          ) :
-          (
-          <span>
-              <Link className ="topnav-link" to="/Login">Log In</Link>
-              <Link className ="topnav-link" to="/Signup">Sign Up</Link>
-          </span>
-          )
-        }
+        </Link>
+        <div className="topnav-right align">
+          {this.props.currentUser ? (
+            <div>
+              <button onMouseDown={this.routeChange.bind(this, "/History")}>
+                History
+              </button>
+              <button onMouseDown={this.routeChange.bind(this, "/Logout")}>
+                Log Out
+              </button>
+              <ShoppingCartButton
+                style={{ top: "50", left: "0" }}
+                itemsInCart={this.props.itemsInCart}
+                onAddToCartClick={this.props.handleAddToCartClick}
+                onRemoveFromCartClick={this.props.handleRemoveFromCartClick}
+              />
+            </div>
+          ) : (
+            <div>
+              <button onMouseDown={this.routeChange.bind(this, "/Login")}>
+                Log In
+              </button>
+              <button onMouseDown={this.routeChange.bind(this, "/Signup")}>
+                Sign Up
+              </button>
+              <ShoppingCartButton
+                style={{ top: "50", left: "0" }}
+                itemsInCart={this.props.itemsInCart}
+                onAddToCartClick={this.props.handleAddToCartClick}
+                onRemoveFromCartClick={this.props.handleRemoveFromCartClick}
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Header;
+    );
+  }
+}
