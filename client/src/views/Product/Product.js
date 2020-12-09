aa9e3b435a0a73b618f037129aeb36daa50ec430
Jiayan Huang <huangjyn@gmail.com>
2020-12-09 01:11:26 -0500

    Added animations to product page, fixed the colors of the laundr bombs, added shadows to bombs  ****I did not test these changes!!! Please test these before merging

--- client/src/views/Product/Product.js
+++ client/src/views/Product/Product.js
@@ -17,6 +17,18 @@ import freshBackground from "./../../assets/backgroundPatterns/freshair_pattern.
 import coffeeBackground from "./../../assets/backgroundPatterns/vanilla_pattern.png";
 import mahoganyBackground from "./../../assets/backgroundPatterns/mahogany_pattern.png";
 
+import watermelonBottom from "./../../assets/detergentImages/watermelon_bottom.png";
+import coffeeBottom from "./../../assets/detergentImages/coffee_bottom.png";
+import eucalyptusBottom from "./../../assets/detergentImages/eucalyptus_bottom.png";
+import freshBottom from "./../../assets/detergentImages/freshair_bottom.png";
+import gardeniaBottom from "./../../assets/detergentImages/gardenia_bottom.png";
+import mahoganyBottom from "./../../assets/detergentImages/mahogany_bottom.png";
+import zipperSideways from "./../../assets/detergentImages/zipper_sideways.png";
+
+import bomb1_5 from "./../../assets/bombs/Laundr Bomb Angle1.5 White.png";
+import bomb1 from "./../../assets/bombs/Laundr Bomb Angle1 White.png";
+import bomb2 from "./../../assets/bombs/Laundr Bomb Angle2 White.png";
+
 export default class Product extends React.Component {
   constructor(props) {
     super(props);
@@ -44,14 +56,14 @@ export default class Product extends React.Component {
       case "watermelon-cucumber":
         this.setState({
           back: watermelonBackground,
-          img: watermelonImg,
+          img: watermelonBottom,
           alt: "watermelon",
         });
         break;
       case "eucalyptus-tea-tree":
         this.setState({
           back: eucalyptusBackground,
-          img: eucalyptusImg,
+          img: eucalyptusBottom,
           alt: "eucalyptus",
         });
 
@@ -59,7 +71,7 @@ export default class Product extends React.Component {
       case "white-gardenia":
         this.setState({
           back: gardeniaBackground,
-          img: gardeniaImg,
+          img: gardeniaBottom,
           alt: "gardenia",
         });
 
@@ -67,7 +79,7 @@ export default class Product extends React.Component {
       case "fresh-air":
         this.setState({
           back: freshBackground,
-          img: freshImg,
+          img: freshBottom,
           alt: "freshair",
         });
 
@@ -75,7 +87,7 @@ export default class Product extends React.Component {
       case "coffee-vanilla":
         this.setState({
           back: coffeeBackground,
-          img: coffeeImg,
+          img: coffeeBottom,
           alt: "coffee",
         });
 
@@ -83,7 +95,7 @@ export default class Product extends React.Component {
       case "mahogany-teakwood":
         this.setState({
           back: mahoganyBackground,
-          img: mahoganyImg,
+          img: mahoganyBottom,
           alt: "mahogany",
         });
 
@@ -141,11 +153,23 @@ export default class Product extends React.Component {
           <div className={this.state.alt + "Background4"}></div>
           <div className={this.state.alt + "Background5"}></div>
           <h1 className={this.state.alt + "Title"}>{item?.metadata.name2}</h1>
+
+          <div class="productpWrapper">
           <img
-            src={this.state.img}
-            alt={item?.metadata.name + " Image"}
-            className="productpImage"
-          ></img>
+            src={zipperSideways} 
+            alt={"Product"}
+            class="productpZipper"
+          />     
+          <img
+            src={img}
+            alt={"Product"}
+            class="productpBag"
+          />
+          </div>
+          <img src={bomb1_5} alt="Bomb Image 1.5" className="Bomb1_5"></img>
+          <img src={bomb1} alt="Bomb Image 1" className="Bomb1"></img>
+          <img src={bomb2} alt="Bomb Image 2" className="Bomb2"></img>
+
           <h1 className={this.state.alt + "Title bottomTitle"}>
             {item?.metadata.name3}
           </h1>
